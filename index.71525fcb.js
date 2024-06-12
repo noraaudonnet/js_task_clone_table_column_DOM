152436a58af824c721eebdbf0a64b3d0cedf0431
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-12 15:38:50 +0300

    make build

---
+++ index.71525fcb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("thead tr"),r=document.querySelector("tfoot tr"),c=e.children[1].cloneNode(!0),o=r.children[1].cloneNode(!0);e.children[3].after(c),r.children[3].after(o),document.querySelectorAll("tbody tr").forEach(function(e){e.children[3].after(e.children[1].cloneNode(!0))});
+//# sourceMappingURL=index.71525fcb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const headRow = document.querySelector("thead tr");
+const footRow = document.querySelector("tfoot tr");
+const headSecondCol = headRow.children[1].cloneNode(true);
+const footSecondCol = footRow.children[1].cloneNode(true);
+headRow.children[3].after(headSecondCol);
+footRow.children[3].after(footSecondCol);
+document.querySelectorAll("tbody tr").forEach((row)=>{
+    row.children[3].after(row.children[1].cloneNode(true));
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
